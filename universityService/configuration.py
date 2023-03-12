bbb874db3baddacec64b2a00546481f6717e5095
Nikhil_B_M <47153228+Nikhil-B-Madhu@users.noreply.github.com>
2023-03-11 23:04:31 -0700

    Kafdrop
    Adding kafdrop for debugging the movement of traffic.

--- universityService/configuration.py
+++ universityService/configuration.py
@@ -8,6 +8,6 @@ class FlaskConfig(object):
 
 
 class DevelopmentConfig(FlaskConfig):
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL", "postgresql://postgres:postgres@0.0.0.0:32768/postgres")
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL", "postgresql://postgres:postgres@localhost:32768/postgres")
     MESSAGE_QUEUE_URI = environ.get("MESSAGE_QUEUE_URI", "amqp://guest:guest@0.0.0.0:5672")
     SECRET_KEY = environ.get("SECRET_KEY", "secret!")
