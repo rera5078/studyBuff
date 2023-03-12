8c21c7c69afbeff7d3404688d5890e4879e827dd
Mia <ming9798@colorado.edu>
2023-03-12 09:21:47 -0600

    connecting pages

--- studyBuffUI/src/App.tsx
+++ studyBuffUI/src/App.tsx
@@ -1,67 +1,70 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Start from "./StartPage/Start";
+import SearchPage from "./SearchPage/SearchPage";
+import NavBar from "./NavBar/NavBar";
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  return (
-      <div className="App gradient-background">
-        <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <p>
-            STUDY <br /> BUFF
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-          </a>
-        </header>
+    const navigate = useNavigate();
+    function navigateToSearchPage() {
+        navigate('/search');
+    }
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<NavBar />}>
+                <Route index element={<Start />} />
+                <Route path="search" element={<SearchPage />} />
+            </Route>
+        </Routes>
 
-          <div className="App-body">
-              <p>The new, easy-to-use, intuitive <br/>
-                  tool to help you find the perfect <br/>
-                  classes to take.
-              </p>
-          </div>
+    return (
 
+            <div className="App gradient-background">
+                <header className="App-header">
+                    <p>
+                        STUDY <br /> BUFF
+                    </p>
+                </header>
 
-          {/*<div className="App-sub-title">*/}
-          {/*    <p>ABOUT STUDY BUFF*/}
-          {/*    </p>*/}
-          {/*</div>*/}
+                <div className="App-body">
+                    <p>The new, easy-to-use, intuitive <br/>
+                        tool to help you find the perfect <br/>
+                        classes to take.
+                    </p>
+                </div>
 
-          <div className="App-sub-body">
-              <p>Study Buff uses intelligent machine <br/>
-                  learning to aggregate data and present <br/>
-                  search results in a user-readable manner. <br/>
-                  All you have to do is enter a search and <br/>
-                  watch the magic happen!
-              </p>
-          </div>
+                <div className="App-sub-body">
+                    <p>Study Buff uses intelligent machine <br/>
+                        learning to aggregate data and present <br/>
+                        search results in a user-readable manner. <br/>
+                        All you have to do is enter a search and <br/>
+                        watch the magic happen!
+                    </p>
+                </div>
 
-          <button className="button">GET STARTED</button>
+                <button className="button" onClick={navigateToSearchPage}> GET STARTED</button>
 
-          <div className="header">
-              <nav>
-                  <a href="#">
-                      {/*<img src={logo} className="App-logo" alt="logo" />*/}
-                      <img src="logo.png" alt="icon" className="icon"/>
-                  </a>
-                  <ul>
-                      <li><a href="#">Home</a></li>
-                      <li><a href="#">Login</a></li>
-                  </ul>
-              </nav>
-          </div>
+                <div className="header">
+                    <nav>
+                        <a href="#">
+                            <img src="logo.png" alt="icon" className="icon"/>
+                        </a>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Login</a></li>
+                        </ul>
+                    </nav>
+                </div>
 
-          <div className="footer">
-              <p> Copyright &copy; 2023 University of Colorado Boulder. All rights reserved.</p>
-          </div>
-
-      </div>
-  );
+                <div className="footer">
+                    <p> Copyright &copy; 2023 University of Colorado Boulder. All rights reserved.</p>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ studyBuffUI/src/StartPage/Start.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Start.css';
+import SearchPage from "../SearchPage/SearchPage";
+import {Link} from "react-router-dom";
+
+function Start() {
+    return (
+        <div className="App gradient-background">
+            <header className="App-header">
+                {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                <p>
+                    STUDY <br /> BUFF
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+            </header>
+
+            <div className="App-body">
+                <p>The new, easy-to-use, intuitive <br/>
+                    tool to help you find the perfect <br/>
+                    classes to take.
+                </p>
+            </div>
+
+
+            {/*<div className="App-sub-title">*/}
+            {/*    <p>ABOUT STUDY BUFF*/}
+            {/*    </p>*/}
+            {/*</div>*/}
+
+            <div className="App-sub-body">
+                <p>Study Buff uses intelligent machine <br/>
+                    learning to aggregate data and present <br/>
+                    search results in a user-readable manner. <br/>
+                    All you have to do is enter a search and <br/>
+                    watch the magic happen!
+                </p>
+            </div>
+
+            <Link to = "../SearchPage/SearchPage">
+                <button className="button"> GET STARTED</button>
+            </Link>
+
+            {/*<button className="button" onClick={SearchPage}>GET STARTED</button>*/}
+
+            <div className="header">
+                <nav>
+                    <a href="#">
+                        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                        <img src="logo.png" alt="icon" className="icon"/>
+                    </a>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="footer">
+                <p> Copyright &copy; 2023 University of Colorado Boulder. All rights reserved.</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Start;
