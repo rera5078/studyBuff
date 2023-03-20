b687a6676c6ad3c7a8c437ef11577d8d22cb8d92
YashwanthSwamy <yasw2875@colorado.edu>
2023-03-20 15:39:19 -0600

    feat: postman collection and Write Course Details To ES url

--- universityService/src/features/featchAndStoreCourseDetails/routes/fetchCoursesRoute.py
+++ universityService/src/features/featchAndStoreCourseDetails/routes/fetchCoursesRoute.py
@@ -9,7 +9,7 @@ class FetchCoursesRoute(MethodView):
     def get(self):
         print(f"Received Fetch Course Request ")
         try:
-            clear_flag = request.args.get('clearFlag', default=False)
+            clear_flag = request.args.get('clearFlag', default=True)
             src_db_id = request.args.get('srcDB', default=None)
             InitiationService.get_course_details(clear_flag, src_db_id)
             return jsonify({"message": "Fetched Successfully"}), 200
