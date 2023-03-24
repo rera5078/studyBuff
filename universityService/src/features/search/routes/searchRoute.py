cf0ff90f0b42882f4c66e56fd752f6c3d056efca
rera5078 <89604299+rera5078@users.noreply.github.com>
2023-03-23 18:17:04 -0600

    end point to search and query elasticsearch

--- universityService/src/features/search/routes/searchRoute.py
+++ universityService/src/features/search/routes/searchRoute.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
 from flask import jsonify, request
-
+from elasticsearch import Elasticsearch 
 from src.features.search.services.initiateSearch import InitiateSearch
 
 
@@ -15,3 +15,17 @@ class SearchRoute(MethodView):
         except Exception:
             print(f"[SearchRoute] Search failed with an exception")
             return jsonify({"message": "Search Failed"}), 400
+
+class SearchES(MethodView):
+    
+    def post(self):
+        print(f"Searching Elasticsearch")
+        try:
+            query = request.args.get('query', default="") 
+            es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
+            res = es.search(index="study_buff", body=query)
+            return [result['_source'] for result in res['hits']['hits']]    
+        
+        except Exception:
+            print(f"[SearchRouteES] Search failed with an exception")
+            return jsonify({"message": "Searching Elasticsearch Failed"}), 400
