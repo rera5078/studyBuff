5fe9b7d38dff6f16d6c83dea55b164ea5f5119f3
rera5078 <89604299+rera5078@users.noreply.github.com>
2023-03-23 18:19:45 -0600

    Update configRoutes.py
    Created new url for elasticsearch

--- universityService/src/routes/configRoutes.py
+++ universityService/src/routes/configRoutes.py
@@ -1,12 +1,15 @@
 from ..features.featchAndStoreCourseDetails.routes.fetchCoursesRoute import FetchCoursesRoute
-from ..features.search.routes.searchRoute import SearchRoute
+from ..features.search.routes.searchRoute import SearchRoute, SearchES
 from ..features.writeCourseDetailsToES.routes.writeCourseDetailsToESRoute import WriteCourseDetailsToESRoute
 
 
 def load_config_routes(app):
     app.add_url_rule("/api/v1/universityService/fetchCourses",
                      view_func=FetchCoursesRoute.as_view("fetch_courses_api"))
+    app.add_url_rule("/api/v1/universityService/searchES",
+                     view_func=SearchES.as_view("searchES_api"))
     app.add_url_rule("/api/v1/universityService/search",
                      view_func=SearchRoute.as_view("search_api"))
     app.add_url_rule("/api/v1/universityService/writeCourseDetailsToES",
                      view_func=WriteCourseDetailsToESRoute.as_view("WriteCourseDetailsToES_api"))
+
