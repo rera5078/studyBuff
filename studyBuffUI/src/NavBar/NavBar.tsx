a07f3b84c81f17f2031f765d1e9e249c3064e781
Mia <ming9798@colorado.edu>
2023-04-13 17:28:43 -0600

    nav bar picture

--- studyBuffUI/src/NavBar/NavBar.tsx
+++ studyBuffUI/src/NavBar/NavBar.tsx
@@ -1,11 +1,18 @@
 import React, { FC } from 'react';
 import './NavBar.css';
+import buff from "../buff.png";
+import {Link} from "react-router-dom";
 
 interface NavBarProps {}
 
+
+
+
 const NavBar: FC<NavBarProps> = () => (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top d-flex custom-nav">
-        <a className="navbar-brand ms-3" style={{color:'#ffffff'}} href="/">Study Buff</a>
+        <a href="/">
+            <img src={buff} className="photo" />
+        </a>
         <div className="collapse navbar-collapse d-flex" id="navbarSupportedContent">
         </div>
         <div className={"justify-content-end"}>
