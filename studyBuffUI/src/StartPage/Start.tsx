fa479eaffc5a2c039405b9bfba4b1f96c507f354
ming9798 <74949057+ming9798@users.noreply.github.com>
2023-04-17 19:28:53 -0600

    Add files via upload

--- studyBuffUI/src/StartPage/Start.tsx
+++ studyBuffUI/src/StartPage/Start.tsx
@@ -1,57 +1,54 @@
 import './Start.css';
 import {Link} from "react-router-dom";
 import buff from '../buff.png'
-import Footer from "../Footer/Footer"
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+import Button from "@mui/material/Button";
+import Footer from "../Footer/Footer";
 
 function Start() {
     return (
-        <div className="gradient-background">
-            <div className='container row d-inline-flex'>
-                <div className='container col-md-6 mt-5 d-inline-flex'>
-                <header className="App-header w-100">
-                {/*<img src={logo} className="App-logo" alt="logo" />*/}
-                <ul className='list'>
-                    <li className='item' style={{letterSpacing:'7px'}}>STUDY</li>
-                    <li style={{letterSpacing:'10px'}}>BUFF</li>
-                </ul>
-            </header>
+        <React.Fragment>
+            <div className='gradient-background'>
+                <h2 className='study'>STUDY</h2>
+                <h2 className='Buff'>BUFF</h2>
             </div>
-            <div className='container col-md-6 d-inline-flex' style={{marginTop:'150px', position:'relative'}}>
-                <ul className = 'list ms-5 me-5 mt-3' style={{listStyleType:'none'}}>
-                    <li className='item2 w-100'>The new, easy-to-use, intuitive
-                    tool <br/> to help you find the perfect
-                    classes to take.</li>
-                    <li className='item2 w-100'>Study Buff uses intelligent machine <br/>
-                    learning to aggregate data and present <br/>
-                    search results in a user-readable manner. <br/>
-                    All you have to do is enter a search and <br/>
-                    watch the magic happen!</li>
-                    <li className='item2 w-100'><Link to = "/search">
-                    <button className="cb2 w-100 mt-4"> GET STARTED</button>
-                    </Link></li>
-                </ul>
+            <div className ='body'>
+                The new, easy-to-use, intuitive
+                tool <br/> to help you find the perfect
+                classes to take. <br/>
+                Study Buff uses intelligent machine <br/>
+                learning to aggregate data and present <br/>
+                search results in a user-readable manner. <br/>
+                All you have to do is enter a search and <br/>
+                watch the magic happen!
+
             </div>
-            </div>
+
+            <div className='start'><Link to = "/search">
+                <button className="start-button"> GET STARTED</button>
+            </Link></div>
+
             <nav className="navbar navbar-expand-lg navbar-light fixed-top d-flex">
                 <img src={buff} className='photo ms-4 mt-2'></img>
-        <div className="collapse navbar-collapse d-flex" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-            </ul>
-        </div>
-        <div className={"justify-content-end"}>
-            <ul className="navbar-nav mr-auto">
-            <li className="nav-item me-4 mt-2">
-                <Link to = "/login">
-                    <button type="button" className="cb">Login</button>
-                </Link>
-            </li>
-            </ul>
-        </div>
-    </nav>
+                <div className="collapse navbar-collapse d-flex" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                    </ul>
+                </div>
+                <div className={"justify-content-end"}>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item me-4 mt-2">
+                            <Link to = "/login">
+                                <button type="button" className="cb">Login</button>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
             <Footer></Footer>
 
-        </div>
-    );
+        </React.Fragment>
+    )
 }
 
 export default Start;
