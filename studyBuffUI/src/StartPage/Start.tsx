2105b99da89f63a6eb9b4c42e229034f60fe0c01
YashwanthSwamy <yasw2875@colorado.edu>
2023-04-22 15:08:25 -0600

    fix: adding tram info

--- studyBuffUI/src/StartPage/Start.tsx
+++ studyBuffUI/src/StartPage/Start.tsx
@@ -1,5 +1,5 @@
 import './Start.css';
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import white from '../white.png'
 import Footer from "../Footer/Footer";
@@ -10,7 +10,23 @@ import {
     MenuItem,
 } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { Box, Typography } from '@mui/material';
 import PersonIcon from "@mui/icons-material/Person";
+import pranjal from "../pranjal.png";
+import nick from "../nick.png";
+import mia from "../mia.png";
+import becca from "../becca.png";
+import tahira from "../tahira.png";
+import yash from "../yash.png";
+
+const teamMembers = [
+    { name: 'Pranjal', imgUrl: pranjal },
+    { name: 'Nikhil', imgUrl: nick },
+    { name: 'Becca', imgUrl: becca },
+    { name: 'Mia', imgUrl: mia },
+    { name: 'Tahira', imgUrl: tahira },
+    { name: 'Yash', imgUrl: yash },
+];
 
 function Start() {
     const navigate = useNavigate();
@@ -41,27 +57,57 @@ function Start() {
     const handleMenuClose = () => {
         setAnchorEl(null);
     };
-    
+
     return (
         <React.Fragment>
             <div className='start-gradient-background'>
-                <h2 className='study'>STUDY</h2>
-                <h2 className='Buff'>BUFF</h2>
-            </div>
-            <div className ='body'>
-                The new, easy-to-use, intuitive
-                tool <br/> to help you find the perfect
-                classes to take. <br/>
-                Study Buff uses intelligent machine <br/>
-                learning to aggregate data and present <br/>
-                search results in a user-readable manner. <br/>
-                All you have to do is enter a search and <br/>
-                watch the magic happen!
+                <div>
+                    <h2 className='study'>STUDY</h2>
+                    <h2 className='Buff'>BUFF</h2>
+                </div>
+                <div className='body'>
+                    <div className='content-info'>
+                        The new, easy-to-use, intuitive
+                        tool <br /> to help you find the perfect
+                        classes to take. <br />
+                        Study Buff uses intelligent machine <br />
+                        learning to aggregate data and present <br />
+                        search results in a user-readable manner. <br />
+                        All you have to do is enter a search and <br />
+                        watch the magic happen!
+                    </div>
+                </div>
+                <div className='team'>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        {teamMembers.map((member) => (
+                            <Box
+                                key={member.name}
+                                textAlign="center"
+                                mx={1}
+                                sx={{
+                                    borderRadius: '50%',
+                                    overflow: 'hidden',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    width: 120, height: 120 
+                                }}
+                            >
+                                <Avatar alt={member.name} src={member.imgUrl} sx={{ width: 80, height: 80 }} />
+                                <Typography variant="subtitle1" sx={{ marginTop: 1, fontWeight: 700 }}>
+                                    {member.name}
+                                </Typography>
+                            </Box>
+                        ))}
+                    </Box>
+                </div>
             </div>
 
-            <div className='start'><Link to = "/search">
-                <button className="start-button"> GET STARTED</button>
-            </Link></div>
+            <div className='start'>
+                <Link to="/search">
+                    <button className="start-button"> GET STARTED</button>
+                </Link>
+            </div>
 
             <nav className="navbar navbar-expand-lg navbar-light fixed-top d-flex">
                 <img src={white} className='photo ms-4 mt-2'></img>
@@ -72,24 +118,24 @@ function Start() {
                 <div className={"justify-content-end"}>
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item me-4 mt-2">
-                        {isLoggedIn ? (
-                            <>
-                                <Button onClick={handleMenuOpen}>
-                                    <Avatar><PersonIcon/></Avatar>
-                                </Button>
-                                <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-                                    <MenuItem>{userInfo?.name}</MenuItem>
-                                    <MenuItem onClick={handleLogout}>
-                                        <span style={{marginRight: "16px"}}>Logout</span>
-                                        <LogoutIcon />
-                                    </MenuItem>
-                                </Menu>
-                            </>
-                        ) : (
-                            <Link to="/login">
-                                <button type="button" className="cb">Login</button>
-                            </Link>
-                        )}
+                            {isLoggedIn ? (
+                                <>
+                                    <Button onClick={handleMenuOpen}>
+                                        <Avatar><PersonIcon /></Avatar>
+                                    </Button>
+                                    <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+                                        <MenuItem>{userInfo?.name}</MenuItem>
+                                        <MenuItem onClick={handleLogout}>
+                                            <span style={{ marginRight: "16px" }}>Logout</span>
+                                            <LogoutIcon />
+                                        </MenuItem>
+                                    </Menu>
+                                </>
+                            ) : (
+                                <Link to="/login">
+                                    <button type="button" className="cb">Login</button>
+                                </Link>
+                            )}
                         </li>
                     </ul>
                 </div>
